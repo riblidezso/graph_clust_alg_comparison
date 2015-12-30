55dd3e62d3733d71fe4ac832645d2eedc82e561e
riblidezso <dkribli@gmail.com>
2015-12-30 16:14:46 +0100

    fixed the benchmark graph generation error

--- my_modules/girwan_newman_benchmark.py
+++ my_modules/girwan_newman_benchmark.py
@@ -17,22 +17,29 @@ def create_GN_benchmark_graph(k_in):
         g.add_nodes_from(nodes)
 
     #add intra partition edges
+    #loop over partitions
     for k in xrange(1,5):
+        # loop over nodes
         for i in xrange(32):
-            for j in xrange(32):
-                if i!=j:
-                    if np.random.random() < p:
-                        g.add_edge(k*100+i,k*100+j)
+            # loop over nodes higher than i
+            # to avoid revisiting an edge
+            # note: xrange gives different result than range here
+            for j in range(i+1,32):
+                if np.random.random() < p:
+                    g.add_edge(k*100+i,k*100+j)
 
     #add cross partition edges
+    #loop over partitions
     for k1 in xrange(1,5):
-        for k2 in xrange(1,5):
+        #loop over partitions higher than k1
+        # to avoid revisiting an edge
+        for k2 in range(k1+1,5):
             if (k1!=k2):
+                #no need to worry about revisiting now
                 for i in xrange(32):
                     for j in xrange(32):
-                        if i!=j:
-                            if np.random.random() < q:
-                                g.add_edge(k1*100+i,k2*100+j)
+                        if np.random.random() < q:
+                            g.add_edge(k1*100+i,k2*100+j)
 
     return g,partitions
 
