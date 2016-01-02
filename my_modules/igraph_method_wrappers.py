436e1b7b82fe38468066299dff8e837df0e0c5f0
riblidezso <dkribli@gmail.com>
2016-01-02 16:05:54 +0100

    started to work on real data

--- my_modules/igraph_method_wrappers.py
+++ my_modules/igraph_method_wrappers.py
@@ -1,7 +1,10 @@
 """Functions to work with igraph methods"""
 
 import igraph
+import networkx as nx
 import numpy as np
+import time
+
 from girwan_newman_benchmark import create_GN_benchmark_graph
 from girwan_newman_benchmark import fraction_of_vertices_correctly_classified
 
@@ -44,7 +47,6 @@ def best_modularity_level_w_igraph_method(graph,igraph_method):
     return best_clusters
 
 
-
 def nx_2_ig(nx_g):
     """
     Covert networkx graph to igraph graph.
@@ -59,15 +61,17 @@ def nx_2_ig(nx_g):
     # but you actually cannot ...
     # so i maintain a name - id mapping
     id_name,name_id=dict(),dict()
-    for node,i in zip(nx_g.nodes(),range(128)):
+    for node,i in zip(nx_g.nodes(),xrange(nx_g.number_of_nodes())):
         ig_g.add_vertex(name=node)
         id_name[i],name_id[node]=node,i
     
-    for edge in nx_g.edges():
-        ig_g.add_edge(name_id[edge[0]],name_id[edge[1]])
- 
+    #add edges in a list, because one by one is very slow
+    ig_g.add_edges([(name_id[edge[0]],name_id[edge[1]]) for edge in nx_g.edges()])
+        
     return ig_g,id_name,name_id
 
+
+"""Functions to test on synthetic benchmark."""
 
 def test_igraph_method(k_in,igraph_method):
     """
@@ -101,3 +105,27 @@ def scan_k_in_vals(igraph_method,N,M,k_in_min=4,k_in_max=16):
     k_in_range=np.linspace(k_in_min,k_in_max,N)
     res = np.array([ repeat_test_igraph_method(k_in,igraph_method,M)  for k_in in k_in_range ])
     return res,k_in_range
+
+
+"""Functions for testing on real data"""
+
+def load_nx_from_edges_file(filename):
+    """ Load networkx graph from a txt file containing edges."""
+    g=nx.Graph()
+    with open('facebook_combined.txt') as file_h:
+        for line in file_h:
+            g.add_edge(line.split()[0],line.split()[1])
+    return g
+
+def load_ig_from_edges_file(filename):
+    """ Load igraph graph from a txt file containing edges."""
+    ig_g,id_name,name_id=nx_2_ig(load_nx_from_edges_file(filename))
+    return ig_g,id_name,name_id
+
+def test_igraph_method_on_real_graph(graph,igraph_method):
+    """Test igraph community detection method on a real graph."""
+    start=time.time()
+    membership=best_modularity_level_w_igraph_method(graph,igraph_method)
+    print 'Modularity:',graph.modularity(membership)
+    print "It took:",time.time()-start,'s'
+    return membership
